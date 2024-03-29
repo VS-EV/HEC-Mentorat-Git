79a4023f561d40952fc176b7f6b65bbfcd2d48c1
Valette Eliott <eliott.valette@hec.edu>
2024-03-29 17:03:28 +0100

    Update Navbar.jsx

--- src/js/Navbar.jsx
+++ src/js/Navbar.jsx
@@ -1,6 +1,6 @@
 // src/js/Navbar.jsx 
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom"
+import { Link , useNavigate } from "react-router-dom"
 
 import '../css/navbar.css'
 
